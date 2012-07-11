4d07fbb39e3f7f2f06bb087aa89d12abe7640632
WojtekM <wojciech.mardyla@gmail.com>
2012-07-11 17:02:24 +0200

    Initial commit fapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fapp::Application.config.secret_token = '62ed298e5c77bc2ed4fa638b27ecfe1b616c51f4d93710efe224b720f879204a08d76325ef898c374a57b4cdc1ab55ef0ebaca2d34230c5be856b6f8cf79bda8'
